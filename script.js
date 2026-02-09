0c224ccc260f003b7a9c17dcab3a22d5eecbdf8e
Jason <jason_ritter@outlook.com>
2026-02-08 19:12:53 -0600

    Add calculateAll key to config object

--- script.js
+++ script.js
@@ -3,29 +3,44 @@
 
 const getElement = (id) => document.getElementById(id);
 const elements = {
-    billAmount: getElement('billAmount'),
-    tipPercentage: getElement('tipPercentage'),
-    numPeople: getElement('numPeople'),
-    calculateBtn: getElement('calculateBtn'),
-    resultsDiv: getElement('results'),
-    tipAmountSpan: getElement('tipAmount'),
-    totalBillSpan: getElement('totalBill'),
-    perPersonSpan: getElement('perPerson')
-}
-const { billAmount, tipPercentage, numPeople, calculateBtn, resultsDiv, tipAmountSpan, totalBillSpan, perPersonSpan } = elements;
+  billAmount: getElement("billAmount"),
+  tipPercentage: getElement("tipPercentage"),
+  numPeople: getElement("numPeople"),
+  calculateBtn: getElement("calculateBtn"),
+  resultsDiv: getElement("results"),
+  tipAmountSpan: getElement("tipAmount"),
+  totalBillSpan: getElement("totalBill"),
+  perPersonSpan: getElement("perPerson"),
+};
 
+const {
+  billAmount,
+  tipPercentage,
+  numPeople,
+  calculateBtn,
+  resultsDiv,
+  tipAmountSpan,
+  totalBillSpan,
+  perPersonSpan,
+} = elements;
 
 // TODO: Create a config object with calculation functions
 // Include: calculateTip, calculateTotal, calculatePerPerson
 // Use arrow functions and spread/rest where appropriate
 
-const config = {
-    calculateTip: (bill, tipPercent) => (bill * tipPercent) / 100,
-    calculateTotal: (bill, tip) => bill + tip,
-    calculatePerPerson: (total, people) => total / people
-}
+const calculator = {
+  calculateTip: (bill, tipPercent) => (bill * tipPercent) / 100,
+  calculateTotal: (bill, tip) => bill + tip,
+  calculatePerPerson: (total, people) => total / people,
 
-const { calculateTip, calculateTotal, calculatePerPerson } = config;
+  calculateAll: (...values) => {
+    const [bill, tipPercent, people] = values;
+    const tip = config.calculateTip(bill, tipPercent);
+    const total = config.calculateTotal(bill, tip);
+    const perPerson = config.calculatePerPerson(total, people);
+    return { tip, total, perPerson };
+  },
+};
 
 // TODO: Create a calculate function that:
 // 1. Gets input values (use destructuring!)
@@ -33,9 +48,7 @@ const { calculateTip, calculateTotal, calculatePerPerson } = config;
 // 3. Performs calculations using spread operator for function arguments
 // 4. Displays results
 
-
 // TODO: Add event listener to calculate button
-
 
 // BONUS: Add real-time calculation on input change
 // BONUS: Add reset button
